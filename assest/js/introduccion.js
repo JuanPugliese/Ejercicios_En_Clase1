ade14ef560439e52b044fd8252ff425b970711ff
JuanPugliese <jcp5432@gmail.com>
2023-11-01 12:48:10 -0500

    Intro a java

---
+++ assest/js/introduccion.js
@@ -0,0 +1,43 @@
+// scripts ECMaScripts isomorfico
+// google chrome v8, motor de ejecucción de java de los navegadores
+// para correr java en back de necesita nodeJS
+
+/*variables
+$name
+_name
+// name
+nombrePersona = Juan;*/
+
+// (let es para declarar variables globales)
+let numero = 1;
+let nombre = 'Juan';
+let miVariable = true;
+let miVariable1 = [1, 'bob'];
+let numero1=2; let numero2=3
+let suma=numero1+numero2;
+let saludo='Hello World'
+pi=3.1416;
+
+console.log(`el numero ${numero1} es muy bonito`)
+
+let a = 5;
+let b = 10;
+console.log(`la suma1 es ${a + b}`)
+console.log(`la suma2 es ${suma}`)
+console.log(`${saludo}`)
+
+/*operadores
+aritmeticos + - * / mod %
+de comparación == === != !== igual, completamente igual, diferente, completamente diferente
+<> <= >=
+lógicos && || !
+console.info()
+console.warn()
+console.error()
+console.clear() ó ctrl + l
+*/
+//ejercicio 1
+
+let num1 = parseInt(prompt("digite el primer número")) ;
+let num2 = parseInt(prompt("digite el segundo número")) ;
+console.log(`la suma1 es ${num1 + num2}`)
